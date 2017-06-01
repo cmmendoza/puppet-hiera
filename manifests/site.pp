4a37112a96b7fdbfa5c49ab78d2904e657b79e45
Charisma Ann Mendoza <cmmendoza@cambridge.org>
2017-06-01 11:29:13 +0800

    initial commit: added manifests

---
+++ manifests/site.pp
@@ -0,0 +1,37 @@
+node 'cmtest-puppetmaster.buildandrelease.org' {
+        class { 'ntp':
+                servers => [ 'ntp1.corp.com', 'ntp2.corp.com' ],
+        }
+#}
+#node 'cmtest-puppetmaster.buildandrelease.org' {
+#        include ntp
+#        package { 'nano':
+#               ensure => '2.3.1',
+#        }
+#        user { 'read only user':
+#               name => 'voyeur',
+#                ensure => 'present',	
+#	}
+	include ntp
+        class { 'profile::release_accounts':
+                ensure => 'present',
+        }
+	include profile::apache
+	include profile::php
+	include profile::mysql
+}
+
+node 'cmtest-puppetslave.buildandrelease.org' {
+	include ::role::amp
+	include ::role::webserver
+}
+
+node 'cha-wordpress-1.buildandrelease.org' {
+	include role::amp
+	include profile::wordpress	
+}
+
+node 'cha-wordpress-2.buildandrelease.org' {
+	include role::amp
+	include profile::wordpress
+}
